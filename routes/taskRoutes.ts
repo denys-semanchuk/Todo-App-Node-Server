f6927f66758b6e757648fbc012d205a193208d45
Denys Semanchuk <semanchukdenys@icloud.com>
2025-02-06 20:33:52 +0100

    added task's routes

---
+++ routes/taskRoutes.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { protect } from "../middleware/protectMiddleware";
+import {
+  getTasks,
+  createTask,
+  deleteTask,
+  updateTask,
+  toggleImportant,
+} from "../controllers/taskController";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/", protect, getTasks);
+router.post("/", protect, createTask);
+router.delete("/:id", protect, deleteTask);
+router.put("/:id", protect, updateTask);
+router.patch("/:id/important", protect, toggleImportant);
+
+export default router;
